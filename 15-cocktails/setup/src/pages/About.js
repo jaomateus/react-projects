226beed8a18d6ebcd9cbe468f27a883194fcffc5
Joao Mateus <jaomateus@gmail.com>
2023-03-07 11:46:07 +0100

    finish exercise

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti magnam voluptatum explicabo sed voluptate iure consectetur corporis dolore? Corporis alias pariatur sapiente explicabo ea aliquid eaque, vero nam cupiditate saepe?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut quo, est optio minima non libero laboriosam dignissimos nemo quae corrupti quod, fugit ipsam? Accusamus labore molestias fugit laudantium dicta quas.
+      </p>
+    </section>
   )
 }
 
