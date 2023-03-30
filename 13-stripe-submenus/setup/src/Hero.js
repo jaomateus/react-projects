e7102a34bfd1d977b502e32cbee6be95b203f036
Joao Mateus <jaomateus@gmail.com>
2023-03-30 22:06:57 +0100

    reset project

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,27 +1,10 @@
 import React from "react";
 import phoneImg from "./images/phone.svg";
-import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const { closeSubmenu } = useGlobalContext();
 
   return (
-    <section className="hero" onMouseHover={closeSubmenu}>
-      <div className="hero-center">
-        <article className="hero-info">
-          <h1>paymentys infrastructure for internet</h1>
-          <p>
-            Millions of comapanies of all sizes-from startups to fortune 500s-
-            use Stripe's software and APIs to accept payments, send payouts, and
-            manage their businesses online.
-          </p>
-          <button className="btn">Start now</button>
-        </article>
-        <article className="hero-images">
-          <img src={phoneImg} className="phone-img" alt="phone" />
-        </article>
-      </div>
-    </section>
+    Hero
   );
 };
 
--- 13-stripe-submenus/setup/src/Submenu.js
+++ 13-stripe-submenus/setup/src/Submenu.js
@@ -1,49 +1,9 @@
-import React, { useState, useRef, useEffect } from "react";
-import { useGlobalContext } from "./context";
+import React, { useState, useRef } from "react";
 
 const Submenu = () => {
-  const {
-    isSubmenuOpen,
-    location,
-    page: { page, links },
-  } = useGlobalContext();
-
-  const container = useRef(null);
-  const [column, setColumns] = useState("col-2");
-
-  useEffect(() => {
-    setColumns("col-2");
-    const submenu = container.current;
-    const { center, bottom } = location;
-    submenu.style.left = `${center}px`;
-    submenu.style.top = `${bottom}`;
-
-    if (links.length === 3) {
-      setColumns("col-3");
-    }
-    if (links.length > 3) {
-      setColumns("col-4");
-    }
-  }, [location, links]);
-
+ 
   return (
-    <aside
-      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
-      ref={container}
-    >
-      <h4>{page}</h4>
-      <div className={`submenu-center ${column}`}>
-        {links.map((link, index) => {
-          const { label, icon, url } = link;
-          return (
-            <a href={url} key={index}>
-              {icon}
-              {label}
-            </a>
-          );
-        })}
-      </div>
-    </aside>
+   Submenu
   );
 };
 
