e83ce8aa6b227e95b6321cfd764aede8f8a91cb7
Joao Mateus <jaomateus@gmail.com>
2023-04-01 19:34:23 +0100

    finish exercise

--- 14-cart/setup/src/App.js
+++ 14-cart/setup/src/App.js
@@ -1,26 +1,12 @@
 import React from "react";
-import { useGlobalContext } from "./context";
 
 // components
 import Navbar from "./Navbar";
 import CartContainer from "./CartContainer";
-// items
 
+// items
 function App() {
-  const { loading } = useGlobalContext();
-  if (loading) {
-    return (
-      <div className='loading'>
-        <h1>Loading...</h1>
-      </div>
-    )
-  }
-  return (
-    <main>
-      <Navbar />
-      <CartContainer />
-    </main>
-  );
+  return 'MyApp'
 }
 
 export default App;
